158c51ae06b579cadcab04b8cea1d1478d1950d9
CodeSH85 <codesh85@gmail.com>
2022-05-11 11:27:36 +0800

    1-2-2 使用 path module

--- hello.js
+++ hello.js
@@ -1,21 +1,22 @@
 // module 模組
 //hello.js
+const path = require('path');
 
 const sayHello = () => {
   console.log('Hello!');
 };
-
-// 輸出模組.模組名稱 = 模組內容
-// 模組名稱與內容不一定需要一至
-module.exports.say = sayHello;
-
-// 物件式寫法（較常用，方便管理）
 
 const sayGoodnight = () => {
   console.log('Good Night!');
 };
 
 const title = 'I am Hello Module!';
+
+// 輸出模組.模組名稱(供外部引用) = 模組內容
+// 模組名稱與內容不一定需要一至
+module.exports.say = sayHello;
+
+// 物件式寫法（較常用，方便管理）
 
 // module.exports = {
 //   sayHello: sayHello,
@@ -29,3 +30,8 @@ module.exports = {
   sayGoodnight,
   title
 };
+
+console.log('module', module);
+console.log('dirname', __dirname);
+console.log('filename',__dirname );
+console.log(path.join(__dirname, 'index.js'));
