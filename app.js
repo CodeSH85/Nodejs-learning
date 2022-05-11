00320c920178bce0ba782dead916b30b7a1d3044
CodeSH85 <codesh85@gmail.com>
2022-05-11 16:23:19 +0800

    2-1-1 使用 Express

--- app.js
+++ app.js
@@ -5,11 +5,11 @@
 
 // 內建模組不需使用檔案路徑，可直接引用
 // const path = require('path');
-const http = require('http');
+// const http = require('http');
 
 // 第二個區塊 第三方模組(套件)
 
-//const express = require('express');
+const express = require('express');
 //const cowsay = require('cowsay');
 
 // 第三個區塊 自建模組
@@ -25,33 +25,64 @@ const http = require('http');
 
 //==================================================
 
-const server = http.createServer((req, res) => {
+//const server = http.createServer((req, res) => {
 	// console.log('第一個參數是瀏覽器對 web server 的 request', req);
 	// console.log('第二個參數是 web 要response 給瀏覽器的內容', res);
 
-  console.log('req url: '+ req.url);
-  if (req.url === '/') {
-    // 指定回傳狀態 http狀態碼 、 MIME type (此處是html格式)
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    // 回傳的網頁內容 務必.end結束回傳
-    return res.end("<h1>This is Home page</h1>");
-  };
-  if (req.url === '/login') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    return res.end("<h1>This is login page</h1>");
-  };
-  res.writeHead(404, { 'Content-Type': 'text/html' });
-  res.end('<h1>page not found :(</h1>');
+//   console.log('req url: '+ req.url);
+//   if (req.url === '/') {
+//     // 指定回傳狀態 http狀態碼 、 MIME type (此處是html格式)
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     // 回傳的網頁內容 務必.end結束回傳
+//     return res.end("<h1>This is Home page</h1>");
+//   };
+//   if (req.url === '/login') {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     return res.end("<h1>This is login page</h1>");
+//   };
+//   res.writeHead(404, { 'Content-Type': 'text/html' });
+//   res.end('<h1>page not found :(</h1>');
+// });
+
+// server.listen(3001, () => {
+// 	console.log('running server on port 3001');
+// });
+
+// // 使用 url 模組來分析 URL
+// const url = require('url');
+
+// console.log(url.parse('https://www.twitch.tv/riotgames'));
+
+// 使用express ========================================
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('this');
+  next();
 });
 
-server.listen(3001, () => {
-	console.log('running server on port 3001');
+app.use((req, res, next) => {
+  console.log('this');
+  next();
 });
 
-// 使用 url 模組來分析 URL
-const url = require('url');
+app.get('/', (req, res) =>{
+  res.writeHead(200,{'Content-Type':'text/html'});
+  res.write('<head><meta charset="utf-8" /><head>');
+  res.write('<body>');
+  res.write('<h1>這是首頁</h1>');
+  res.write('</body>');
+});
 
-console.log(url.parse('https://www.twitch.tv/riotgames'));
+app.get('/login', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<head><meta charset="utf-8" /></head>')
+  res.write('<body>')
+  res.write('<h1>這是 Login 頁</h1>')
+  res.write('</body>')
+});
 
-//==========================================
-
+app.listen(3001, () => {
+  console.log('running server on port 3001');
+});
