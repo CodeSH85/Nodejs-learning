d928eb1c115cd90d74000894854ec3dccc8658a3
CodeSH85 <codesh85@gmail.com>
2022-05-11 11:35:56 +0800

    補充：coding style

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 //console.log('hello,world');
 
 // require(url): 自路徑載入模組，可將模組路徑給予變數方便使用
-// hello.say(): 使用 hello 中的 say()方法 
+// 第一個區塊 內建模組
 
+// 內建模組不需使用檔案路徑，可直接引用
+const path = require('path');
+const http = require('http');
+
+// 第二個區塊 第三方模組(套件)
+
+// 第三個區塊 自建模組
 const hello = require('./hello');
+
+//===============================================
+
+// hello.sayHello(): 使用 hello模組 中的 sayHello()方法 
 
 hello.sayHello();
 
