74c45b3f27ea30f7e4d42d53a7bca57de7b9f904
CodeSH85 <codesh85@gmail.com>
2022-05-12 13:44:25 +0800

    4-2 於模板中帶入參數

--- app-ejs.js
+++ app-ejs.js
@@ -1,4 +1,3 @@
-//console.log('hello,world');
 
 // require(url): 自路徑載入模組，可將模組路徑給予變數方便使用
 // 第一個區塊 內建模組
@@ -18,45 +17,6 @@ const bodyParser = require('body-parser');
 
 //===============================================
 
-// hello.sayHello(): 使用 hello模組 中的 sayHello()方法
-
-//hello.sayHello();
-
-//console.log(hello.title);
-
-//==================================================
-
-// *.原生 Node js 作法
-
-//const server = http.createServer((req, res) => {
-	// console.log('第一個參數是瀏覽器對 web server 的 request', req);
-	// console.log('第二個參數是 web 要response 給瀏覽器的內容', res);
-
-//   console.log('req url: '+ req.url);
-//   if (req.url === '/') {
-//     // 指定回傳狀態 http狀態碼 、 MIME type (此處是html格式)
-//     res.writeHead(200, { 'Content-Type': 'text/html' });
-//     // 回傳的網頁內容 務必.end結束回傳
-//     return res.end("<h1>This is Home page</h1>");
-//   };
-//   if (req.url === '/login') {
-//     res.writeHead(200, { 'Content-Type': 'text/html' });
-//     return res.end("<h1>This is login page</h1>");
-//   };
-//   res.writeHead(404, { 'Content-Type': 'text/html' });
-//   res.end('<h1>page not found :(</h1>');
-// });
-
-// server.listen(3001, () => {
-// 	console.log('running server on port 3001');
-// });
-
-// // 使用 url 模組來分析 URL
-// const url = require('url');
-
-// console.log(url.parse('https://www.twitch.tv/riotgames'));
-
-// *.使用Express套件
 
 // 使用express
 const app = express();
@@ -82,13 +42,18 @@ app.get('/', (req, res) =>{
   // response 回應 http status 200
   res.status(200)
   // .sendFile(path.join(__dirname, 'views', 'index.html'));
-  .render('index');
+  // 在 EJX 中帶入參數
+  .render('index',{
+    pageTitle: 'Book Your Books online'
+  });
 });
 
 app.get('/login', (req, res) => {
   res.status(200)
   //.sendFile(path.join(__dirname, 'views', 'login.html'));
-  .render('login');
+  .render('login',{
+    pageTitle: 'Login Page' 
+  });
 });
 
 app.post('/login',(req, res) => {
@@ -107,10 +72,11 @@ app.post('/login',(req, res) => {
 app.get('*', (req, res) => {
   res.status(404)
   // .sendFile(path.join(__dirname, 'views', '404.html'));
-  .render('404');
+  .render('404',{
+    pageTitle: '404 Not Found' 
+  });
 });
 
 app.listen(3001, () => {
   console.log('running server on port 3001');
 });
-
